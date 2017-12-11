91a9dda333f836efcb7d4c6e6f80ec378052bccf
lml19960131 <lml19960131@126.com>
2017-12-11 15:30:40 +0800

    singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,83 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer">
+    <ul v-for="item in singers">
+      <li>
+        <img :src="item.picture"  width="60" height="60" class="singer-picture">
+        <span class="singer-name">{{item.name}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+  export default{
+    data(){
+      return{
+        singers:[
+          {
+            name: 0,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 1,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 2,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 3,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 4,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 5,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 6,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 7,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 8,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 8,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 8,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          },
+          {
+            name: 8,
+            picture: 'http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg'
+          }
+        ]
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .singer{
+    position: fixed;
+    top: 88px;
+    bottom: 0;
+    width: 100%;
+    overflow-y: auto;
+  }
 
+  .singer-picture{
+    border-radius: 50%;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Recommend from 'components/recommend/recommend.vue'
 import Singer from 'components/singer/singer.vue'
 import Rank from 'components/rank/rank.vue'
 import Search from 'components/search/search.vue'
+// import SingerDetail from  'components/singer-detail/singer-detail'
 
 Vue.use(Router);
 
@@ -16,7 +17,13 @@ export default new Router({
     },
     {
       path: '/singer',
-      component: Singer
+      component: Singer,
+      // children:[
+      //   {
+      //     path: ':id',
+      //     component: SingerDetail
+      //   }
+      // ]
     },
     {
       path: '/rank',
