a994a853210a5a0cc972e4e94158a4a73d877352
lml19960131 <lml19960131@126.com>
2018-01-20 20:31:04 +0800

    slider组件优化及改写为stylus

--- src/baseComponents/slider/slider.vue
+++ src/baseComponents/slider/slider.vue
@@ -41,44 +41,36 @@
         this._setSliderWidth();
         this._initDots();
         this. _initSlider();
-        if (this.autoPlay){
-            this._play()
+        if(this.autoPlay){
+          this._play()
         }
       }, 20);
-      window.addEventListener('resize',()=>{
-        if(!this.slider){
-            return
-        }
-        this._setSliderWidth(true);
-        this.slider.refresh()
+      window.addEventListener('resize', () => {
+          if(!this.slider){
+              return
+          }
+          this._setSliderWidth(true);
+          this.slider.refresh();
       })
-    },
-    activated() {
-      if (this.autoPlay) {
-        this._play()
-      }
-    },
-    deactivated() {
-      clearTimeout(this.timer)
-    },
-    beforeDestroy() {
-      clearTimeout(this.timer)
     },
     methods: {
       _setSliderWidth(isResize){
         this.children = this.$refs.sliderGroup.children;
-        let width=0;
+        let width = 0;
         let sliderWidth = this.$refs.slider.clientWidth;
-        for (let i=0;i<this.children.length;i++){
+        for (let i = 0; i < this.children.length; i++) {
           let child = this.children[i];
-          addClass(child,'slider-item');
+          addClass(child, 'slider-item');
           child.style.width = sliderWidth + 'px';
-          width += sliderWidth
+          width += sliderWidth;
         }
-        if(this.loop &&!isResize){
-          width += 2 * sliderWidth
+        if (this.loop && !isResize) {
+          width += 2 * sliderWidth;
         }
-        this.$refs.sliderGroup.style.width = width + 'px'
+        this.$refs.sliderGroup.style.width = width + 'px';
+      },
+      _initDots(){
+        this.dots = new Array(this.children.length);
       },
       _initSlider(){
         this.slider = new BScroll(this.$refs.slider, {
@@ -86,39 +78,31 @@
           scrollY: false,
           momentum: false,
           snap: true,
-          snapLoop: this.snap,
+          snapLoop: this.loop,
           snapThreshold: 0.3,
-          snapSpeed: 400
+          snapSpeed: 400,
         });
         this.slider.on('scrollEnd', () => {
           let pageIndex = this.slider.getCurrentPage().pageX;
-          if(this.loop){
-            pageIndex -= 1;
-          }
           this.currentPageIndex = pageIndex;
-          if (this.autoPlay){
-            clearTimeout(this.timer);
-            this._play()
+          if(this.autoPlay){
+              clearTimeout(this.timer);
+              this._play();
           }
         });
-        this.slider.on('beforeScrollStart', () => {
-          if (this.autoPlay) {
-            clearTimeout(this.timer)
-          }
-        })
-      },
-      _initDots(){
-        this.dots = new Array(this.children.length)
       },
       _play(){
-        let pageIndex =this.currentPageIndex+1;
-        if(this.loop){
-          pageIndex += 1;
-        }
-        this.timer=setTimeout(() =>{
-          this.slider.goToPage(pageIndex, 0, 400)
-        }, this.interval)
+          let pageIndex = this.currentPageIndex + 1;
+          if(pageIndex >= this.$refs.sliderGroup.children.length){
+              pageIndex = 0;
+          }
+          this.timer = setTimeout(() =>{
+              this.slider.goToPage(pageIndex, 0, 400);
+          }, this.interval)
       }
+    },
+    destroyed(){
+        clearTimeout(this.timer);
     }
   }
 </script>
@@ -126,56 +110,41 @@
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import '../../../src/common/stylus/variable.styl';
 
-  .slider{
-    min-height: 1px;
-  }
-
-  .slider-group{
-    position: relative;
-    overflow: hidden;
-    white-space: nowrap;
-  }
-
-  .slider-item{
-    float: left;
-    box-sizing: border-box;
-    overflow: hidden;
-    text-align: center;
-  }
-
-  a{
-    display: block;
-    width: 100%;
-    overflow: hidden;
-    text-decoration: none;
-  }
-
-  img{
-    display: block;
-    width: 100%;
-  }
-
-  .dots{
-    position: absolute;
-    right: 0;
-    left: 0;
-    bottom: 12px;
-    text-align: center;
-    font-size: 0;
-  }
-
-  .dot{
-    display: inline-block;
-    margin: 0 4px;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background: $color-text-l;
-  }
-
-  .dots > .active{
-    width: 20px;
-    border-radius: 5px;
-    background: $color-text-ll;
-  }
+  .slider
+    min-height 1px
+    .slider-group
+      position relative
+      overflow hidden
+      white-space nowrap
+      .slider-item
+        float left
+        box-sizing border-box
+        overflow hidden
+        text-align center
+        a
+          display block
+          width 100%
+          overflow hidden
+          text-decoration none
+        img
+          display block
+          width 100%
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      text-align: center
+      font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 15px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
