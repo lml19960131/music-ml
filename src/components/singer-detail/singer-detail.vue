22fb5089278c9612d3e6a46efcda1eb35fc4788c
lml19960131 <lml19960131@126.com>
2018-01-22 21:59:47 +0800

    singer-detail组件

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,70 @@
+<template>
+  <transition name="slider">
+    <div class="singer-detail"></div>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  import {mapGetters} from 'vuex'
+  import {getSingerDetail} from '../../../src/api/singer'
+  import {ERR_OK} from '../../../src/api/config'
+  import {createSong} from '../../../src/common/js/song'
+
+  export default {
+    data(){
+      return{
+        song: []
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'singer'
+      ])
+    },
+    created() {
+      this._getDetail();
+    },
+    methods: {
+      _getDetail() {
+        if(!this.singer.id){
+          this.$router.push('/singer');
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if(res.code === ERR_OK){
+            this.songs = this._nomalizeSongs(res.data.list);
+            console.log(this.songs)
+          }
+        })
+      },
+      _nomalizeSongs(list){
+        let ret = [];
+        list.forEach((item) => {
+          let {musicData} = item;
+          if(musicData.songid && musicData.albummid){
+            ret.push(createSong(musicData))
+          }
+        });
+        return ret;
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/variable.styl'
+
+  .singer-detail
+    position fixed
+    z-index 100
+    top 0
+    right 0
+    bottom 0
+    left 0
+    background $color-background
+
+  .slider-enter-active,.slider-leave-active
+    transition all 0.3s
+  .slider-enter,.slider-leave-to
+    transform translate3d(100%, 0, 0)
+</style>
