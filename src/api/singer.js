f91076bad1c33e439eaf6dd70871614096173a1b
lml19960131 <lml19960131@126.com>
2018-01-22 21:52:30 +0800

    歌曲抓取

--- src/api/singer.js
+++ src/api/singer.js
@@ -18,3 +18,22 @@ export function getSingerList() {
 
   return jsonp(url, data, options);
 }
+
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg';
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 1,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 150,
+    songstatus: 1,
+    singermid: singerId,
+    g_tk:5381
+  });
+  return jsonp(url, data, options)
+}
+
