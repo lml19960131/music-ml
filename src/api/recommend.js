7144868e336595ad8d23a372cb9f9be2e8c18525
lml19960131 <lml19960131@126.com>
2018-01-24 21:25:09 +0800

    recommend在线获取数据

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,11 +2,18 @@ import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
 export function getRecommend() {
-  const url = 'https://c.y.qq.com/rsc/fcgi-bin/fcg_get_profile_homepage.fcg';
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
   const data = Object.assign({},commonParams,{
     platform: 'h5',
     uin: 0,
-    needNewCode: 1
+    needNewCode: 1,
+    g_tk: 5381,
+    format: 'json'
   });
   return jsonp(url, data, options)
+}
+
+
+export default {
+  
 }
