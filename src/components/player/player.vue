dd4a20bc292897ee5636a6b9705c9363a99adec3
lml19960131 <lml19960131@126.com>
2018-01-25 23:27:18 +0800

    play组件

---
+++ src/components/player/player.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="player" v-show="playlist.length>0">
+    <div class="normal-player" v-show="fullScreen">
+      播放器
+    </div>
+    <div class="mini-player" v-show="!fullScreen"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {mapGetters} from 'vuex'
+
+  export default{
+    computed: {
+      ...mapGetters([
+        'fullScreen',
+        'playlist'
+      ])
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
+  @import "../../common/stylus/mixin.styl"
+
+  .play
+    .normal-player
+      position fixed
+      top 0
+      right 0
+      bottom 0
+      left 0
+      z-index 150
+      background $color-background
+    .mini-player
+      display: flex
+      align-items: center
+      position: fixed
+      left: 0
+      bottom: 0
+      z-index: 180
+      width: 100%
+      height: 60px
+      background: $color-highlight-background
+</style>
