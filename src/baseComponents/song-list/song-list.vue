a29bc835ac97441b86edc13a95fe44b145e7eace
lml19960131 <lml19960131@126.com>
2018-01-25 23:27:45 +0800

    song-list组件

--- src/baseComponents/song-list/song-list.vue
+++ src/baseComponents/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="song-list">
     <ul>
-      <li v-for="song in songs" class="item">
+      <li v-for="(song, index) in songs" class="item" @click="selectItem(song, index)">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -22,6 +22,9 @@
     methods:{
       getDesc(song){
         return `${song.singer}·${song.album}`
+      },
+      selectItem(item, index){
+        this.$emit('select', item ,index);
       }
     }
   }
