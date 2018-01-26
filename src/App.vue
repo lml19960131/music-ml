5758f1170983e46f40d0f4303a10daf04ca69087
lml19960131 <lml19960131@126.com>
2018-01-26 23:31:30 +0800

    Player组件引入

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,21 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <player></player>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import myHeader from 'components/head/my-header.vue'
   import tab from 'components/tab/tab.vue'
+  import Player from 'components/player/player.vue'
 
   export default{
     name: 'app',
     components:{
       myHeader,
-      tab
+      tab,
+      Player
     }
   }
 </script>
