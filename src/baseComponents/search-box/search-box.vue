0601399077d78509d9ffc347aea16e8c99a47cd2
lml19960131 <lml19960131@126.com>
2018-02-01 16:48:51 +0800

    search-box(搜索框)组件

---
+++ src/baseComponents/search-box/search-box.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="box" :placeholder="placeholder" v-model="query"/>
+    <i class="icon-dismiss" v-show="query" @click="clear"></i>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      placeholder: {
+        type: String,
+        default: '搜索歌曲、歌手'
+      }
+    },
+    data() {
+      return {
+        query: ''
+      }
+    },
+    created() {
+      this.$watch('query', (newQuery) => {
+        this.$emit('query', newQuery)
+      })
+    },
+    methods: {
+      clear() {
+        this.query = ''
+      },
+      setQuery(query) {
+        this.query = query
+      }
+    }
+  }
+</script>
+
+<style scoped lang='stylus' rel="stylesheet/stylus">
+  @import '../../common/stylus/variable.styl'
+
+  .search-box
+    display flex
+    align-items center
+    box-sizing border-box
+    width 100%
+    padding 0 6px
+    height 46px
+    background $color-highlight-background
+    border-radius 6px
+    .icon-search
+      font-size 24px
+      color $color-background
+    .box
+      flex 1
+      margin 0 5px
+      background $color-highlight-background
+      color $color-text
+      font-size $font-size-medium
+      &::placeholder
+        color $color-text-d
+    .icon-dismiss
+      font-size 16px
+      color $color-background
+</style>
