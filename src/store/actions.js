f03771944d0ebea4915eaf30745a863f4f6159dc
lml19960131 <lml19960131@126.com>
2018-02-02 22:47:29 +0800

    insert状态

--- src/store/actions.js
+++ src/store/actions.js
@@ -31,3 +31,36 @@ export const randomPlay = function ({commit}, {list}) {
   commit(types.SET_FULL_SCREEN, true);
   commit(types.SET_PLAYING_STATE, true);
 };
+
+export const insertSong = function ({commit, state}, song) {
+  let playlist = state.playlist.slice();
+  let sequencelist = state.sequenceList.slice();
+  let currentIndex= state.currentIndex;
+  let currentSong = playlist[currentIndex];//记录当前歌曲
+  let findEqualIndex = findIndex(playlist, song); //判断当前列表中是否有要插入的歌曲
+  currentIndex++;
+  playlist.splice(currentIndex, 0, song);//插入歌曲到初始位置
+  if(findEqualIndex > -1) {
+    if(currentIndex > findEqualIndex) {
+      playlist.splice(findEqualIndex, 1);
+      currentIndex--
+    } else {
+      playlist.splice(findEqualIndex + 1, 1);
+    }
+  }//判断插入的播放位置和插入位置的关系，并删除已有的歌曲
+  let currentSIndex = findIndex(sequencelist, currentSong) + 1;//获取插入位置
+  let findESIndex = findIndex(sequencelist, song);
+  sequencelist.splice(currentSIndex, 0, song);
+  if(findESIndex > -1){
+    if(currentSIndex > findESIndex){
+      sequencelist.splice(findESIndex, 1);
+    } else {
+      sequencelist.splice(findESIndex+1, 1);
+    }
+  }
+  commit(types.SET_PLAYLIST, playlist);
+  commit(types.SET_SEQUENCE_LIST,sequencelist);
+  commit(types.SET_CURRENT_INDEX, currentIndex);
+  commit(types.SET_FULL_SCREEN, true);
+  commit(types.SET_PLAYING_STATE, true);
+};
