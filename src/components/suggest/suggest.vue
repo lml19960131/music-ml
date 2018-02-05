8aa745741c908e0538c26eab535854d0154d5cf5
lml19960131 <lml19960131@126.com>
2018-02-05 20:53:31 +0800

    suggest组件

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -150,6 +150,9 @@
       listScroll() {
         this.$emit('listScroll')
       },
+      refresh() {
+        this.$refs.suggest.refresh()
+      },
       ...mapMutations({
         setSinger: 'SET_SINGER'
       }),
