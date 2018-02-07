99212a966a373dd3ee46b744b4e96e39f9144345
lml19960131 <lml19960131@126.com>
2018-02-07 20:11:30 +0800

    mixin混入共用的vue方法和计算属性

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -1,4 +1,7 @@
-import {mapGetters} from 'vuex'
+import {mapGetters, mapMutations} from 'vuex'
+import {playMode} from '../../common/js/config'
+import {shuffle} from '../../common/js/util'
+
 
 export const playlistMixin = {
   computed: {
@@ -23,3 +26,39 @@ export const playlistMixin = {
     }
   }
 };
+
+export const playerMixin = {
+  computed: {
+    iconMode() {
+      return this.mode === playMode.sequence ? 'icon-sequence' : this.mode === playMode.loop ? 'icon-loop' :
+        'icon-random';
+    }
+  },
+  methods: {
+    changeMode() {
+      const mode = (this.mode + 1) % 3;
+      this.setPlayMode(mode);
+      let list = null;
+      if(mode === playMode.random) {
+        list = shuffle(this.sequenceList);
+      } else {
+        list = this.sequenceList;
+      }
+      this.resetCurrentIndex(list);
+      this.setPlaylist(list);
+    },
+    resetCurrentIndex(list) {
+      let index = list.findIndex((item) => {
+        return item.id === this.currentSong.id;
+      });
+      this.setCurrentIndex(index);
+    },
+    ...mapMutations({
+      setFullScreen: 'SET_FULL_SCREEN',
+      setPlayingState: 'SET_PLAYING_STATE',
+      setCurrentIndex: 'SET_CURRENT_INDEX',
+      setPlayMode: 'SET_PLAY_MODE',
+      setPlaylist: 'SET_PLAYLIST'
+    }),
+  }
+}
