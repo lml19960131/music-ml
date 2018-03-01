2b9fc521358deaff24b0e353c50565f7bc3dec19
lml19960131 <lml19960131@126.com>
2018-03-01 23:17:04 +0800

    player组件stylus书写改

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -307,182 +307,182 @@
       z-index 150
       background $color-background
       .background
-        position: absolute
-        left: 0
-        top: 0
-        width: 100%
-        height: 100%
-        z-index: -1
-        opacity: 0.6
-        filter: blur(20px)
+        position absolute
+        left 0
+        top 0
+        width 100%
+        height 100%
+        z-index -1
+        opacity 0.6
+        filter blur(20px)
       .top
-        position: relative
-        margin-bottom: 25px
+        position relative
+        margin-bottom 25px
         .back
           position absolute
-          top: 0
-          left: 6px
-          z-index: 50
+          top 0
+          left 6px
+          z-index 50
           .icon-back
-            display: block
-            padding: 9px
-            font-size: $font-size-large-x
-            color: $color-theme
-            transform: rotate(-90deg)
+            display block
+            padding 9px
+            font-size $font-size-large-x
+            color $color-theme
+            transform rotate(-90deg)
         .title
-          width: 70%
-          margin: 0 auto
-          line-height: 40px
-          text-align: center
+          width 70%
+          margin 0 auto
+          line-height 40px
+          text-align center
           no-wrap()
-          font-size: $font-size-large
-          color: $color-text
+          font-size $font-size-large
+          color $color-text
         .subtitle
-          line-height: 20px
-          text-align: center
-          font-size: $font-size-medium
-          color: $color-text
+          line-height 20px
+          text-align center
+          font-size $font-size-medium
+          color $color-text
       .middle
-        position: fixed
-        width: 100%
-        top: 80px
-        bottom: 170px
-        white-space: nowrap
-        font-size: 0
+        position fixed
+        width 100%
+        top 80px
+        bottom 170px
+        white-space nowrap
+        font-size 0
         .middle-l
-          display: inline-block
-          vertical-align: top
-          position: relative
-          width: 100%
-          height: 0
-          padding-top: 80%
+          display inline-block
+          vertical-align top
+          position relative
+          width 100%
+          height 0
+          padding-top 80%
           .cd-wrapper
-            position: absolute
-            left: 10%
-            top: 0
-            width: 80%
-            height: 100%
+            position absolute
+            left 10%
+            top 0
+            width 80%
+            height 100%
             .cd
-              width: 100%
-              height: 100%
-              box-sizing: border-box
-              border: 10px solid rgba(255, 255, 255, 0.1)
-              border-radius: 50%
+              width 100%
+              height 100%
+              box-sizing border-box
+              border 10px solid rgba(255, 255, 255, 0.1)
+              border-radius 50%
               &.play
-                animation: rotate 20s linear infinite
+                animation rotate 20s linear infinite
               &.pause
-                animation-play-state: paused
+                animation-play-state paused
               .image
-                position: absolute
-                left: 0
-                top: 0
-                width: 100%
-                height: 100%
-                border-radius: 50%
+                position absolute
+                left 0
+                top 0
+                width 100%
+                height 100%
+                border-radius 50%
       .bottom
-        position: absolute
-        bottom: 50px
-        width: 100%
+        position absolute
+        bottom 50px
+        width 100%
         .progress-wrapper
-          display: flex
-          align-items: center
-          width: 80%
-          margin: 0 auto
-          padding: 10px 0
+          display flex
+          align-items center
+          width 80%
+          margin 0 auto
+          padding 10px 0
           .time
-            color: $color-text
-            font-size: $font-size-small
-            flex: 0 0 30px
-            line-height: 30px
-            width: 30px
+            color $color-text
+            font-size $font-size-small
+            flex 0 0 30px
+            line-height 30px
+            width 30px
             &.time-l
-              text-align: left
+              text-align left
             &.time-r
-              text-align: right
+              text-align right
           .progress-bar-wrapper
-            flex: 1
+            flex 1
         .operators
-          display: flex
-          align-items: center
+          display flex
+          align-items center
           .icon
-            flex: 1
-            color: $color-theme
+            flex 1
+            color $color-theme
             &.disable
-              color: $color-theme-d
+              color $color-theme-d
             i
-              font-size: 30px
+              font-size 30px
           .i-left
-            text-align: right
+            text-align right
           .i-center
-            padding: 0 20px
-            text-align: center
+            padding 0 20px
+            text-align center
             i
-              font-size: 40px
+              font-size 40px
           .i-right
-            text-align: left
+            text-align left
           .icon-favorite
-            color: $color-sub-theme
+            color $color-sub-theme
       &.normal-enter-active, &.normal-leave-active
-        transition: all 0.4s
+        transition all 0.4s
         .top, .bottom
-          transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+          transition all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
       &.normal-enter, &.normal-leave-to
-        opacity: 0
+        opacity 0
         .top
-          transform: translate3d(0, -100px, 0)
+          transform translate3d(0, -100px, 0)
         .bottom
-          transform: translate3d(0, 100px, 0)
+          transform translate3d(0, 100px, 0)
     .mini-player
       display: flex
-      align-items: center
-      position: fixed
-      left: 0
-      bottom: 0
-      z-index: 180
-      width: 100%
-      height: 60px
-      background: $color-highlight-background
+      align-items center
+      position fixed
+      left 0
+      bottom 0
+      z-index 180
+      width 100%
+      height 60px
+      background $color-highlight-background
       &.mini-enter-active, &.mini-leave-active
-        transition: all 0.4s
+        transition all 0.4s
       &.mini-enter, &.mini-leave-to
       .icon
-        flex: 0 0 40px
-        width: 40px
-        padding: 0 10px 0 20px
+        flex 0 0 40px
+        width 40px
+        padding 0 10px 0 20px
         img
-          border-radius: 50%
+          border-radius 50%
           &.play
             animation: rotate 10s linear infinite
           &.pause
-            animation-play-state: paused
+            animation-play-state paused
       .text
-        display: flex
-        flex-direction: column
-        justify-content: center
-        flex: 1
-        line-height: 20px
-        overflow: hidden
+        display flex
+        flex-direction column
+        justify-content center
+        flex 1
+        line-height 20px
+        overflow hidden
         .name
-          margin-bottom: 2px
+          margin-bottom 2px
           no-wrap()
-          font-size: $font-size-medium
-          color: $color-text
+          font-size $font-size-medium
+          color $color-text
         .desc
           no-wrap()
-          font-size: $font-size-small
-          color: $color-text-d
+          font-size $font-size-small
+          color $color-text-d
       .control
-        flex: 0 0 30px
-        width: 30px
-        padding: 0 10px
+        flex 0 0 30px
+        width 30px
+        padding 0 10px
         .icon-play-mini, .icon-pause-mini, .icon-playlist
-          font-size: 30px
-          color: $color-theme-d
+          font-size 30px
+          color $color-theme-d
         .icon-mini
-          font-size: 32px
-          position: absolute
-          left: 0
-          top: 0
+          font-size 32px
+          position absolute
+          left 0
+          top 0
 
   @keyframes rotate
     0%
